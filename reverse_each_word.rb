5e1365fcb1f2b240f0e49f94c24cde53f03054fb
Julianne Kim <juliannne@gmail.com>
2017-10-18 22:45:21 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,12 @@
+# def reverse_each_word(sentence)
+#   reversed = []
+#   sentence.split.each do |word|
+#     reversed << word.reverse
+#   end
+#   reversed.join(" ")
+# end
+
 def reverse_each_word(sentence)
-  reversed = []
-  sentence.split.each do |word|
-    reversed << word.reverse
-  end
-  reversed.join(" ")
+  sentence.split.collect { |word| word.reverse }
 end
+
